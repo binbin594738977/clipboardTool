795a709b57b00647fbeac8ed82b42871d7d0c691
fuheng <594738977@qq.com>
2020-10-15 10:55:10 +0800

    修复

--- src/code/Test.java
+++ src/code/Test.java
@@ -1,10 +1,21 @@
 package code;
 
-import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        JProgressBar jProgressBar = new JProgressBar(0, 100);
-        jProgressBar.setVisible(true);
+        System.out.println("start");
+        Map<Long, Long> map = new HashMap<>();
+        final long max = (1L << 22);
+        long time = System.currentTimeMillis();
+        for (long i = 0; i < max; i++) {
+            if (i % 0x400000 == 0) {
+                System.out.println(i + " / " + max + " -> " + ((i * 100) / max) + "%");
+            }
+            long val = (long) (Math.random() * max) * 128L;
+            map.put(val, val);
+        }
+        System.out.println("use " + (System.currentTimeMillis() - time));
     }
 }
